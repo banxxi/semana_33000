ca214360e26b26f4bf441894bb35271172b4b589
hellen <lozanohellen511@gmail.com>
2025-09-02 12:13:26 -0500

    subiendo ejercicio 1

---
+++ semana1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;//detenimiento de consola
+            Console.Write("ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese su carrera:");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"\n{nombre},bienvenido al curso"+ $"de fundamentos de algoritmo {carrera}");
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ semana1_pyton/semana1_pyton.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre=input("ingrese su nombre:")
+    carrera= input ("ingrese su carrera:")
+
+    print(f"\n{nombre}, bienvenido al curso de fundamentos algoritmo de la carerera{carrera}") 
+
+ejer1()
